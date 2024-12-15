358d3fb81c22c8009aeac5281a1ec3b460a6f456
houfio <github@houf.io>
2024-12-15 23:13:30 +0100

    Refactor day 5

--- day05/index.ts
+++ day05/index.ts
@@ -1,36 +1,25 @@
 import { readLines, run } from '../utils.ts';
 
-function findAndSum(
-  input: string[],
-  fn: (line: number[], isValid: (line: number[]) => number[][]) => number[] | undefined
-) {
-  const halfway = input.indexOf('');
-  const rules = input.slice(0, halfway).map((line) => line.split('|').map(Number));
-  const lines = input.slice(halfway + 1).map((line) => line.split(',').map(Number));
-
-  const getInvalid = (line: number[]) =>
-    line.flatMap((value, index) => {
-      const before = line.slice(0, index);
-      const disallowed = rules.filter((rule) => rule[0] === value);
-
-      return disallowed.filter((d) => before.includes(d[1]));
-    });
-
-  return lines.reduce((previous, current) => {
-    const sum = fn(current, getInvalid);
-
-    return previous + (sum ? sum[Math.floor(sum.length / 2)] : 0);
-  }, 0);
-}
-
 function findValid(fix: boolean) {
   return (input: string[]) => {
-    return findAndSum(input, (line, getInvalid) => {
-      if (!fix) {
-        return !getInvalid(line).length ? line : undefined;
-      }
+    const halfway = input.indexOf('');
+    const rules = input.slice(0, halfway).map((line) => line.split('|').map(Number));
+    const lines = input.slice(halfway + 1).map((line) => line.split(',').map(Number));
 
+    const getInvalid = (line: number[]) =>
+      line.flatMap((value, index) => {
+        const before = line.slice(0, index);
+        const disallowed = rules.filter((rule) => rule[0] === value);
+
+        return disallowed.filter((d) => before.includes(d[1]));
+      });
+
+    const valid = lines.map((line) => {
       let invalid = getInvalid(line);
+
+      if (!fix) {
+        return invalid.length ? undefined : line;
+      }
 
       if (!invalid.length) {
         return;
@@ -48,6 +37,8 @@ function findValid(fix: boolean) {
 
       return line;
     });
+
+    return valid.reduce((previous, current) => previous + (current ? current[Math.floor(current.length / 2)] : 0), 0);
   };
 }
 
