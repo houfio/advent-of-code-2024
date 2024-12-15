f35e60087e90b7f8a787853db5d53742d9ff1b12
houfio <github@houf.io>
2024-12-15 19:51:37 +0100

    Combine functions of day 1

--- day01/index.ts
+++ day01/index.ts
@@ -1,18 +1,18 @@
 import { readNumberGrid, run } from '../utils.ts';
 
-function calculateDistance(input: number[][]) {
-  const leftSorted = input.flatMap((row) => row[0]).toSorted();
-  const rightSorted = input.flatMap((row) => row[1]).toSorted();
+function calculateResult(similarity: boolean) {
+  return (input: number[][]) => {
+    const left = input.flatMap((row) => row[0]).toSorted();
+    const right = input.flatMap((row) => row[1]).toSorted();
 
-  return leftSorted.reduce((previous, current, index) => previous + Math.abs(current - rightSorted[index]), 0);
+    if (!similarity) {
+      return left.reduce((previous, current, index) => previous + Math.abs(current - right[index]), 0);
+    }
+
+    const scores = left.map((l) => right.filter((r) => r === l).length * l);
+
+    return scores.reduce((previous, current) => previous + current, 0);
+  }
 }
 
-function calculateSimilarity(input: number[][]) {
-  const left = input.flatMap((row) => row[0]);
-  const right = input.flatMap((row) => row[1]);
-  const scores = left.map((value) => right.filter((v) => v === value).length * value);
-
-  return scores.reduce((previous, current) => previous + current, 0);
-}
-
-await run(() => readNumberGrid('   '), [calculateDistance, calculateSimilarity]);
+await run(() => readNumberGrid('   '), [calculateResult(false), calculateResult(true)]);
