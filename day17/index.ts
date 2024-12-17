395b1a962499b85ca85f84becc04176f6bf177ba
houfio <github@houf.io>
2024-12-17 19:39:56 +0100

    Finish day 17

--- day17/index.ts
+++ day17/index.ts
@@ -1,76 +1,63 @@
-import { mod, readLines, run } from '../utils.ts';
+import { readText, run } from '../utils.ts';
 
-type Register = [number, number, number];
-type Fn = (value: number) => void;
-type InstructionInput = { literal: number; combo: number; register: Register; jump: Fn; output: Fn };
-type Instruction = (input: InstructionInput) => void;
+function execute(find: boolean) {
+  return (input: string) => {
+    const [a, b, c, ...program] = input.match(/\d+/g)?.map(Number) ?? [];
+    const loop = (register: [bigint, bigint, bigint]) => {
+      const output: number[] = [];
 
-const instructions: Record<number, Instruction> = {
-  0: ({ combo, register }) => {
-    register[0] = Math.floor(register[0] / 2 ** combo);
-  },
-  1: ({ literal, register }) => {
-    register[1] = register[1] ^ literal;
-  },
-  2: ({ combo, register }) => {
-    register[1] = mod(combo, 8);
-  },
-  3: ({ literal, register, jump }) => {
-    if (register[0]) {
-      jump(literal);
-    }
-  },
-  4: ({ register }) => {
-    register[1] = register[1] ^ register[2];
-  },
-  5: ({ combo, output }) => {
-    output(mod(combo, 8));
-  },
-  6: ({ combo, register }) => {
-    register[1] = Math.floor(register[0] / 2 ** combo);
-  },
-  7: ({ combo, register }) => {
-    register[2] = Math.floor(register[0] / 2 ** combo);
-  }
-};
+      for (let pointer = 0; pointer < program.length; pointer += 2) {
+        const [instruction, literal] = program.slice(pointer, pointer + 2);
+        const combo = BigInt(literal > 3 && literal < 7 ? register[literal - 4] : literal);
 
-function execute(input: string[]) {
-  const numbers = input.filter(Boolean).flatMap((line) =>
-    line
-      .slice(line.indexOf(':') + 2)
-      .split(',')
-      .map(Number)
-  );
-  const register = numbers.slice(0, 3) as Register;
-  const program = numbers.slice(3);
-  const output: number[] = [];
-  let pointer = 0;
-
-  while (pointer < program.length) {
-    const instruction = instructions[program[pointer]];
-    const literal = program[pointer + 1];
-    const combo = literal > 3 ? (literal > 6 ? -1 : register[literal - 4]) : literal;
-    let jumped = false;
-
-    instruction({
-      literal,
-      combo,
-      register,
-      jump: (value) => {
-        pointer = value;
-        jumped = true;
-      },
-      output: (value) => {
-        output.push(value);
+        switch (instruction) {
+          case 0:
+            register[0] = register[0] / 2n ** combo;
+            break;
+          case 1:
+            register[1] = register[1] ^ BigInt(literal);
+            break;
+          case 2:
+            register[1] = combo % 8n;
+            break;
+          case 3:
+            pointer = register[0] ? literal - 2 : pointer;
+            break;
+          case 4:
+            register[1] = register[1] ^ register[2];
+            break;
+          case 5:
+            output.push(Number(combo % 8n));
+            break;
+          case 6:
+            register[1] = register[0] / 2n ** combo;
+            break;
+          case 7:
+            register[2] = register[0] / 2n ** combo;
+            break;
+        }
       }
-    });
 
-    if (!jumped) {
-      pointer += 2;
+      return output.join(',');
+    };
+
+    if (!find) {
+      return loop([BigInt(a), BigInt(b), BigInt(c)]);
     }
-  }
 
-  return output.join(',');
+    const target = program.join(',');
+    let result = 0n;
+
+    for (let i = program.length - 1; i >= 0; i--) {
+      result <<= 3n;
+
+      while (!target.endsWith(loop([result, BigInt(b), BigInt(c)]))) {
+        result++;
+      }
+    }
+
+    return Number(result);
+  };
 }
 
-await run(readLines, [execute]);
+await run(readText, [execute(false), execute(true)]);
